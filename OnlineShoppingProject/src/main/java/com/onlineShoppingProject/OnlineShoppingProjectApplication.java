36b424146bb86b0b701c02fad1f7d1ec3bd54526
a121 <atulpisal.ap@gmail.com>
2019-08-20 18:24:44 +0530

    initial time

---
+++ OnlineShoppingProject/src/main/java/com/onlineShoppingProject/OnlineShoppingProjectApplication.java
@@ -0,0 +1,14 @@
+package com.onlineShoppingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShoppingProjectApplication.class, args);
+		System.out.println("done : ");
+	}
+
+}
---
+++ OnlineShoppingProject/src/test/java/com/onlineShoppingProject/OnlineShoppingProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.onlineShoppingProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlineShoppingProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
